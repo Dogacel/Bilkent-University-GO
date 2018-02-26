bf16ccb2ad8f62411199eb60a7e1783c85183d49
dogacel <dogacel@gmail.com>
2018-02-27 00:30:02 +0300

    Experimental location and sensor tests.

--- app/src/main/java/ar/kudan/eu/kudansimple/MainActivity.java
+++ app/src/main/java/ar/kudan/eu/kudansimple/MainActivity.java
@@ -4,6 +4,14 @@ import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,12 +26,15 @@ import android.view.MenuItem;
 
 import eu.kudan.kudan.ARAPIKey;
 import eu.kudan.kudan.ARActivity;
+import eu.kudan.kudan.ARGyroPlaceManager;
 
-public class MainActivity extends ARActivity {
+public class MainActivity extends ARActivity implements SensorEventListener, LocationListener{
 
     public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
     public static final String ALLOW_KEY = "ALLOWED";
     public static final String CAMERA_PREF = "camera_pref";
+
+    private static String accelData, gyroData, compassData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +42,8 @@ public class MainActivity extends ARActivity {
         String skey = "<agWZcpYLYjBxCbWf2qZx6k+PWISqeGtFCqKaZwYtwS+kdn1HKiQAmsJ55STRBe9BqCw3VwG6qL+ESI5ntTF/iV/uekLG3PCokaUE0/uTzqhaYlxRdmuNBIduzBCjq3mV2na+gy3ffHH9Ipc7eIN0geTj3p+ppsmK0U399iGmN38ndIh6k2y16cByWIecMSU3yw3Ztw7gHRqf83hVhZ5T2ACGK4SNkQhhdKp+CTaR5W3amYCJBgwumqFqNFyI9UniuMk70T/cQObRQum2U51OjjbMfmEAwIBt8Q8jD2yACzye6K4/1O4pZhbGEbiDeLrAfxqMwBAe5o6vnYIilGNnpDhfi3wOHhRaqtLOVvB58GUIFTnAPvmYFVnLWRJmCUZ9FJNDyX3ALCl/alFEWh+A/a6NFjcwLGKI9drPuGG4ONFg4p0l+p3b9DZoLzszlmWAflI/UFzQa++kQn3/sclO9i0vPnpi0LWoABm5vGswLVAIX/0k6384GXxfkADI6fjGtf62XJ5ImaVDiiREa9mabWEQGoifghQG1sGNDYgBIYEpiaLsVzOfTALpe20Q7kFCMjedJImQhhuLtEK1BXfXJEed1QqUOsG9IeKxKk28GbOtOF9w3yrSF3gnJslzZxF2kEF3C6ckog8byagS+4p37FJmbpPsiKNH1Qm0LuouGcQ=>";
         ARAPIKey key = ARAPIKey.getInstance();
         key.setAPIKey(skey);
-
-
     }
 
-    @Override
-    public void onRequestPermissionsResult
-            (int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-
-        }
-    }
 
     @Override
     public void setup()
@@ -60,8 +56,78 @@ public class MainActivity extends ARActivity {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.CAMERA},
                     MY_PERMISSIONS_REQUEST_CAMERA);
+
         }
 
+
         // AR Content to be set up here
+        ARGyroPlaceManager gyroPlaceManager = ARGyroPlaceManager.getInstance();
+        gyroPlaceManager.initialise();
+
+        SensorManager sensors = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accelSensor = sensors.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        Sensor compassSensor = sensors.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor gyroSensor = sensors.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        boolean isAccelAvailable = sensors.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        boolean isCompassAvailable = sensors.registerListener(this, compassSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        boolean isGyroAvailable = sensors.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.NO_REQUIREMENT);
+
+        String best = locationManager.getBestProvider(criteria, true);
+
+        Log.v("DEBUG","Best provider: " + best);
+
+        locationManager.requestLocationUpdates(best, 50, 0, this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        StringBuilder msg = new StringBuilder(event.sensor.getName()).append(" ");
+        for(float value: event.values)
+        {
+            msg.append("[").append(value).append("]");
+        }
+        // Log.d("SENSOR", msg.toString());
+        switch(event.sensor.getType())
+        {
+            case Sensor.TYPE_ACCELEROMETER:
+                accelData = msg.toString();
+                break;
+            case Sensor.TYPE_GYROSCOPE:
+                gyroData = msg.toString();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                compassData = msg.toString();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor var1, int var2){
+        // Nothing
+    }
+
+    private Location lastLocation = null;
+
+    public void onLocationChanged(Location location) {
+        lastLocation = location;
+    }
+
+    public void onProviderDisabled(String provider) {
+        // ...
+    }
+
+    public void onProviderEnabled(String provider) {
+        // ...
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // ...
     }
 }
