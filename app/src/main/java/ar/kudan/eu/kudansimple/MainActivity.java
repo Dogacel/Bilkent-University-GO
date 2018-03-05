841d214698f14ad03d585c4d2b88ec697a34b3a7
Dogacel <dogacel@gmail.com>
2018-03-05 14:15:51 +0300

    Updated to basic app demo.

--- app/src/main/java/ar/kudan/eu/kudansimple/MainActivity.java
+++ app/src/main/java/ar/kudan/eu/kudansimple/MainActivity.java
@@ -17,9 +17,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -30,7 +33,7 @@ import eu.kudan.kudan.ARArbiTrack;
 import eu.kudan.kudan.ARGyroPlaceManager;
 import eu.kudan.kudan.ARImageNode;
 
-public class MainActivity extends ARActivity implements SensorEventListener, LocationListener{
+public class MainActivity extends ARActivity implements SensorEventListener, LocationListener, GestureDetector.OnGestureListener{
 
     public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
     public static final String ALLOW_KEY = "ALLOWED";
@@ -40,8 +43,9 @@ public class MainActivity extends ARActivity implements SensorEventListener, Loc
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        gestureDetector = new GestureDetectorCompat(this, this);
         super.onCreate(savedInstanceState);
-        String skey = "<agWZcpYLYjBxCbWf2qZx6k+PWISqeGtFCqKaZwYtwS+kdn1HKiQAmsJ55STRBe9BqCw3VwG6qL+ESI5ntTF/iV/uekLG3PCokaUE0/uTzqhaYlxRdmuNBIduzBCjq3mV2na+gy3ffHH9Ipc7eIN0geTj3p+ppsmK0U399iGmN38ndIh6k2y16cByWIecMSU3yw3Ztw7gHRqf83hVhZ5T2ACGK4SNkQhhdKp+CTaR5W3amYCJBgwumqFqNFyI9UniuMk70T/cQObRQum2U51OjjbMfmEAwIBt8Q8jD2yACzye6K4/1O4pZhbGEbiDeLrAfxqMwBAe5o6vnYIilGNnpDhfi3wOHhRaqtLOVvB58GUIFTnAPvmYFVnLWRJmCUZ9FJNDyX3ALCl/alFEWh+A/a6NFjcwLGKI9drPuGG4ONFg4p0l+p3b9DZoLzszlmWAflI/UFzQa++kQn3/sclO9i0vPnpi0LWoABm5vGswLVAIX/0k6384GXxfkADI6fjGtf62XJ5ImaVDiiREa9mabWEQGoifghQG1sGNDYgBIYEpiaLsVzOfTALpe20Q7kFCMjedJImQhhuLtEK1BXfXJEed1QqUOsG9IeKxKk28GbOtOF9w3yrSF3gnJslzZxF2kEF3C6ckog8byagS+4p37FJmbpPsiKNH1Qm0LuouGcQ=>";
+        String skey = "jZGcfDzRtoNpR6eCGWzQalY7udjOAIHrdIXwiQTFopkqjf9bh0nORhUoH/kS1Y28yDZNKKlM5kYjLCNuBx772wZbSQpyS+3AIqr10vEDMbOyJi3pzsAzim7o6zw6dT4rFDqN0BTAivKULfJol3sbJhyp+PWSjZKN/wLKoDzRspL24JTboYqJ1SXsseSTaoqDqdnrTZ2R0TKKciGlxYy3HB+Js8l9miRVGGk6p8Y0bkldc54gl5Bj2txesMqXhBE8nY1RKpVHZsmJxKEDntPjCvWYtZwNIlAMO+Q3XLvbNOU+XFCbvHcPILH3wX6jkIKd93xGtd08hp0lz1yr7rAuamF/hezem9LsCgXQqYJt8WnQTt1soKHcc3Wt7GnTFn/8CBCF6P6NQGlXeyixNcz4L42hkJAFfb+k0pBNu25eQloCirbAr3gEIXy/yYMYxtvRjKOo6X3JLgtZtMqZFLd8ygGgArlvi/c3PiBwgSLtl949ihRVmfPybclIb42enKCWDu4xRHl5mHDH4Cs6giWMURRQltl3bSRhb7IB3FVyVUitTXxV/pUSb8ywuVjla8KoJFsHOdFDKZrbDfWoQmCiYE7/KozlVjRdkajmEY00Uvgbvn+9NDuSItS8vzTtMmbiqIdMpdSCzBgeEkhe9k+F1zkdkeN40YOQsxVV4lT3Zh0=";
         ARAPIKey key = ARAPIKey.getInstance();
         key.setAPIKey(skey);
     }
@@ -51,6 +55,7 @@ public class MainActivity extends ARActivity implements SensorEventListener, Loc
     public void setup()
     {
         super.setup();
+        Log.d("IMPO", "Startdgc");
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -92,8 +97,9 @@ public class MainActivity extends ARActivity implements SensorEventListener, Loc
 
         // Create a node to be used as the target.
         ARImageNode targetNode = new ARImageNode("Cow Target.png");
+        ARImageNode trackingNode = new ARImageNode("Cow Tracking.png");
 
-        // Add it to the Gyro Placement Manager's world so that it moves with the device's Gyroscope.
+        // Add it to the Gyro Placement Manager's world so that  it moves with the device's Gyroscope.
         gyroPlaceManager.getWorld().addChild(targetNode);
 
         // Rotate and scale the node to ensure it is displayed correctly.
@@ -101,11 +107,58 @@ public class MainActivity extends ARActivity implements SensorEventListener, Loc
         targetNode.rotateByDegrees(180.0f, 0.0f, 1.0f, 0.0f);
 
         targetNode.scaleByUniform(0.3f);
-        targetNode.setPosition(1.0f, 1.0f, 1.0f);
+        // targetNode.setPosition(20.0f, 20.0f, 20.0f);
         // Set the ArbiTracker's target node.
+
         arArbiTrack.setTargetNode(targetNode);
-        arArbiTrack.getTargetNode().setVisible(true);
+
+        trackingNode.rotateByDegrees(90.0f, 1.0f, 0.0f, 0.0f);
+        trackingNode.rotateByDegrees(180.0f, 10.0f, 1.0f, 0.0f);
+
+        arArbiTrack.getWorld().addChild(trackingNode);
+
+
     }
+
+    private GestureDetectorCompat gestureDetector;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        ARArbiTrack arbiTrack = ARArbiTrack.getInstance();
+        if (arbiTrack.getIsTracking()) {
+
+            arbiTrack.stop();
+            arbiTrack.getTargetNode().setVisible(true);
+        }
+
+        else {
+            arbiTrack.start();
+            arbiTrack.getTargetNode().setVisible(false);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {return false;}
+
+    @Override
+    public void onShowPress(MotionEvent e) {}
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float f1, float f2) {return false;}
+
+    @Override
+    public void onLongPress(MotionEvent e) {}
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float f1, float f2) {return false;}
 
     @Override
     public void onSensorChanged(SensorEvent event) {
