054c9bf568612dc36a2927ba5d2a4df418ea4361
Faruk <farukbalci99@hotmail.com>
2018-04-01 17:28:17 +0300

    Implement SensorEventListener. Gets orientationValues and Logs them.

--- app/src/main/java/ar/kudan/eu/kudansimple/MainActivity.java
+++ app/src/main/java/ar/kudan/eu/kudansimple/MainActivity.java
@@ -37,7 +37,7 @@ import eu.kudan.kudan.ARGyroPlaceManager;
 import eu.kudan.kudan.ARImageNode;
 import eu.kudan.kudan.ARWorld;
 
-public class MainActivity extends ARActivity implements GestureDetector.OnGestureListener {
+public class MainActivity extends ARActivity implements GestureDetector.OnGestureListener, SensorEventListener {
 
     public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
     public static final String ALLOW_KEY = "ALLOWED";
@@ -45,12 +45,60 @@ public class MainActivity extends ARActivity implements GestureDetector.OnGestur
 
     private static String accelData, gyroData, compassData;
 
+    private SensorManager mSensorManager;
+
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorMagnetometer;
+
+    private float[] mAccelerometerData = new float[3];
+    private float[] mMagnetometerData = new float[3];
+
+
+    /*
+     * (index 0) Azimuth : The direction (north/south/east/west) the device is pointing. 0 is magnetic north.
+     * (index 1) Pitch : The top-to-bottom tilt of the device. 0 is flat.
+     * (index 2) Roll : The left-to-right tilt of the device. 0 is flat.
+     * All three angles are measured in radians, and range from -π (-3.141) to π.
+     *
+     * gets updated when onSensorChanged(SensorEvent event) is called.
+     */
+    float orientationValues[] = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String skey = "jZGcfDzRtoNpR6eCGWzQalY7udjOAIHrdIXwiQTFopkqjf9bh0nORhUoH/kS1Y28yDZNKKlM5kYjLCNuBx772wZbSQpyS+3AIqr10vEDMbOyJi3pzsAzim7o6zw6dT4rFDqN0BTAivKULfJol3sbJhyp+PWSjZKN/wLKoDzRspL24JTboYqJ1SXsseSTaoqDqdnrTZ2R0TKKciGlxYy3HB+Js8l9miRVGGk6p8Y0bkldc54gl5Bj2txesMqXhBE8nY1RKpVHZsmJxKEDntPjCvWYtZwNIlAMO+Q3XLvbNOU+XFCbvHcPILH3wX6jkIKd93xGtd08hp0lz1yr7rAuamF/hezem9LsCgXQqYJt8WnQTt1soKHcc3Wt7GnTFn/8CBCF6P6NQGlXeyixNcz4L42hkJAFfb+k0pBNu25eQloCirbAr3gEIXy/yYMYxtvRjKOo6X3JLgtZtMqZFLd8ygGgArlvi/c3PiBwgSLtl949ihRVmfPybclIb42enKCWDu4xRHl5mHDH4Cs6giWMURRQltl3bSRhb7IB3FVyVUitTXxV/pUSb8ywuVjla8KoJFsHOdFDKZrbDfWoQmCiYE7/KozlVjRdkajmEY00Uvgbvn+9NDuSItS8vzTtMmbiqIdMpdSCzBgeEkhe9k+F1zkdkeN40YOQsxVV4lT3Zh0=";
         ARAPIKey key = ARAPIKey.getInstance();
         key.setAPIKey(skey);
+
+
+        //Get sensor manegers and sensors
+        mSensorManager = (SensorManager) getSystemService(
+                Context.SENSOR_SERVICE);
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_ACCELEROMETER);
+        mSensorMagnetometer = mSensorManager.getDefaultSensor(
+                Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Listeners for the sensors are registered in this callback and
+        // can be unregistered in onStop().
+        //
+        // Check to ensure sensors are available before registering listeners.
+        // Both listeners are registered with a "normal" amount of delay
+        // (SENSOR_DELAY_NORMAL).
+        if (mSensorAccelerometer != null) {
+            mSensorManager.registerListener(this, mSensorAccelerometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorMagnetometer != null) {
+            mSensorManager.registerListener(this, mSensorMagnetometer,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 
     GestureDetectorCompat gestureDetector;
@@ -107,6 +155,15 @@ public class MainActivity extends ARActivity implements GestureDetector.OnGestur
          **/
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Unregister all sensor listeners in this callback so they don't
+        // continue to use resources when the app is stopped.
+        mSensorManager.unregisterListener(this);
+    }
+
     //Testing
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -147,4 +204,35 @@ public class MainActivity extends ARActivity implements GestureDetector.OnGestur
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        int sensorType = event.sensor.getType();
+
+        switch (sensorType) {
+            case Sensor.TYPE_ACCELEROMETER:
+                mAccelerometerData = event.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mMagnetometerData = event.values.clone();
+                break;
+            default:
+                return;
+        }
+
+        float[] rotationMatrix = new float[9];
+        boolean rotationOK = SensorManager.getRotationMatrix(rotationMatrix,
+                null, mAccelerometerData, mMagnetometerData);
+
+        orientationValues = new float[3];
+        if (rotationOK) {
+            SensorManager.getOrientation(rotationMatrix, orientationValues);
+        }
+
+        Log.d("ORIENTATION", "Azimuth: "+ orientationValues[0] + " - Pitch: " + orientationValues[1] + " - Roll: " + orientationValues[2] );
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 }
