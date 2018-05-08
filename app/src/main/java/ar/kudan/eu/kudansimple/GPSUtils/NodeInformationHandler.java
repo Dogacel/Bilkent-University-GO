218732372be3329db2c0d4457238653b5e2da6a6
Faruk <farukbalci99@hotmail.com>
2018-05-08 04:59:10 +0300

    Add map.geojson to res/raw folder. Start implementing NodeInformationHandler class.

---
+++ app/src/main/java/ar/kudan/eu/kudansimple/GPSUtils/NodeInformationHandler.java
@@ -0,0 +1,60 @@
+package ar.kudan.eu.kudansimple.GPSUtils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import ar.kudan.eu.kudansimple.R;
+
+/**
+ * Created by Faruk Balcı on 7.5.2018.
+ * Retrieves ARNode informtaion from json file.
+ */
+
+public class NodeInformationHandler {
+
+    private static int BUFFER_SIZE = 1024;
+    private static String TAG = "NODE_INFO";
+
+    /**
+     * gets JSON string from resources.
+     */
+    private String getJSONString( Context context) {
+        InputStream is;
+        Writer writer;
+        Reader reader;
+        char[] buffer;
+        int n;
+
+        is = context.getResources().openRawResource(R.raw.map);
+        buffer = new char[BUFFER_SIZE];
+        reader = new BufferedReader(new InputStreamReader( is));
+        writer = new StringWriter();
+
+        try{
+            while ( ( n = reader.read( buffer)) != -1 ) {
+                writer.write(buffer, 0, n);
+            }
+        } catch ( IOException e) {
+            Log.e( TAG, "IOExeption while reading map.geojson", e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return writer.toString();
+    }
+
+}
